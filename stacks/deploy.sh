f47dcec770d10a4e40baccbbaa41b5209ce9fd8e
jlaamanen <joona.laamanen@gmail.com>
2018-10-29 23:34:25 +0200

    Fixed a typo in deploy script

--- stacks/deploy.sh
+++ stacks/deploy.sh
@@ -4,5 +4,5 @@
 for DIR in */ ; do
   # Remove last character '/'
   STACK_NAME=${DIR%?}
-  docker deploy -c ${STACK_NAME}/stack.yml ${STACK_NAME}
+  docker stack deploy -c ${STACK_NAME}/stack.yml ${STACK_NAME}
 done
